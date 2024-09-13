8ec222e882a2a84c5e1b4a422159f208ecd77173
ZeranaIslam <zeranaislam123@gmail.com>
2024-09-13 20:16:42 +0600

    Create qsolve2.py

---
+++ qsolve2.py
@@ -0,0 +1,11 @@
+2.Write a program that asks the user to enter a single character and determines whether it is a vowel or a consonant.
+
+def vowelOrConsonant(x): 
+  
+    if (x == 'a' or x == 'e' or
+        x == 'i' or x == 'o' or x == 'u'): 
+        print("Vowel") 
+    else: 
+        print("Consonant") 
+  
+vowelOrConsonant('d')
